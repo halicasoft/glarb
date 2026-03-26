3f483732ea0e3471db105e1f7424e65c1b9ba783
Emi <emilia.green63@gmail.com>
2026-03-26 20:27:59 +0000

    gruul smile

--- scryfall.py
+++ scryfall.py
@@ -31,15 +31,28 @@ class Scryfall(commands.Cog):
         await ctx.send("**" + response["name"] + "**", file=image_file)
 
     
-    @commands.command(name="commander")
+    @commands.group()
     async def commander(self, ctx):
-        response = await self.bot.http_get_json(url + "random?q=is%3Acommander")
+        if ctx.invoked_subcommand is None:
+            # default behavior is fetch any commander
+            response = await self.bot.http_get_json(url + "random?q=is%3Acommander")
+
+            image_url = response["image_uris"]["normal"]
+            image_response = await self.bot.http_get_img(image_url)
+            image_file = discord.File(io.BytesIO(image_response), filename="card.png")
+
+            await ctx.send("From my orb, I fetch the following commander:")
+            await ctx.send("**" + response["name"] + "**", file=image_file)
+    
+    @commander.command(name="gruul")
+    async def gruul(self, ctx): 
+        response = await self.bot.http_get_json(url + "random?q=id%3DGR+is%3Acommander")
 
         image_url = response["image_uris"]["normal"]
         image_response = await self.bot.http_get_img(image_url)
         image_file = discord.File(io.BytesIO(image_response), filename="card.png")
 
-        await ctx.send("From my orb, I fetch the following commander:")
+        await ctx.send("I look into the swirling portents, and I fetch the following Gruul commander:")
         await ctx.send("**" + response["name"] + "**", file=image_file)
 
 
